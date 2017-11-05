535099291cdad98d820b05c48ed6e8d3192325a1
Vlad Ilnitskiy <vlad.ilnitskiy@competera.net>
2017-11-05 14:29:45 +0200

    minor fixes

--- utils/reorder.py
+++ utils/reorder.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('/home/ilnitskiy/work/MRO/mro/results/carlislebelts.csv', 'r') as infile, \
+with open('/home/ilnitskiy/work/MRO/mro/results/carlislebelts1.csv', 'r') as infile, \
         open('/home/ilnitskiy/work/MRO/mro/results/carlislebelts_reordered.csv', 'a') as outfile:
     fieldnames = ['ids', 'catalog_number', 'description', 'image', 'brochure', 'product_specs', 'additional_description']
-    writer = csv.DictWriter(outfile, fieldnames=fieldnames, extrasaction='ignore')
+    writer = csv.DictWriter(outfile, fieldnames=fieldnames)
     writer.writeheader()
     for row in csv.DictReader(infile):
         # writes the reordered rows to the new file
