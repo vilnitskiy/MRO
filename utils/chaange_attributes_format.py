8ebcfb85568cba0a606928cdffe65f64446a6344
Herich <andrey.herasko@gmail.com>
2017-11-19 00:38:37 +0200

    some fix

--- utils/chaange_attributes_format.py
+++ utils/chaange_attributes_format.py
@@ -7,9 +7,9 @@ from scrapy.selector import Selector
 import lxml.html
 import lxml.html.clean as clean
 
-out = pd.read_csv("results/Martin/martin_attributes.csv", sep=',')
+out = pd.read_csv("results/Reelcraft/reelcraft_attributes_table.csv", sep=',')
 catalog_numbers = list(out.catalog_number)
-ids = list(out.ids)
+ids = list(out.id)
 attributes = list(out.attributes)
 catalog_ids = dict(zip(catalog_numbers, ids))
 catalog_attributes = dict(zip(catalog_numbers, attributes))
@@ -26,16 +26,18 @@ def custom_extractor(response, expression):
     data = response.xpath(expression).extract_first()
     return data if data else ' '
 
-with open('results/Martin/martin_attributes_new.csv', 'wb') as csvfile:
+with open('results/Reelcraft/reelcraft_attributes.csv', 'wb') as csvfile:
 	spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"')
 	spamwriter.writerow(['id', 'catalog_number', 'attributes'])
 	for item in catalog_numbers:
 		if str(catalog_attributes[item]) != 'nan':
 			attr = ''
-			html = remove_html_attributes(catalog_attributes[item])
-			html = html.replace('<span>', '').replace('</span>', '')
+			#html = remove_html_attributes(catalog_attributes[item])
+			html = catalog_attributes[item].replace('<font>', '').replace('</forn>', '').replace('<b>', '').replace('</b>', '')
 			content = Selector(text=html)
 			for tr in content.xpath('//table/tr'):
+				if custom_extractor(tr, './td[2]/text()') == ' ':
+					continue
 				attr += tr.xpath('./td[1]/text()').extract_first()+':'+custom_extractor(tr, './td[2]/text()') +"|"
 			spamwriter.writerow([catalog_ids[item], item, attr[:-1]])
 
